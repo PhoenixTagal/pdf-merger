d6756201d661c5eae5f254035656f888efa44942
Phoenix Tagal <116997916+PhoenixTagal@users.noreply.github.com>
2023-09-25 12:39:35 -0700

    Update save_inspections.py
    created code that skips files if they already exist.

--- save_inspections.py
+++ save_inspections.py
@@ -12,26 +12,30 @@ source_directories = {'1551_2023_q3': r'C:\Users\ptagal\OneDrive - Adaptive Biot
                       'bothell_2023_q3': r'C:\Users\ptagal\OneDrive - Adaptive Biotechnologies\Environmental Health and Safety\500 Environmental\510 Regulated Waste Management\Hazardous Waste\Inspection Forms\Eyewash & Shower Inspections\Bothell\2023\Q3',
                       'bothell_2023_q4': r'C:\Users\ptagal\OneDrive - Adaptive Biotechnologies\Environmental Health and Safety\500 Environmental\510 Regulated Waste Management\Hazardous Waste\Inspection Forms\Eyewash & Shower Inspections\Bothell\2023\Q4'}
 
-destination_true_north_inspection_directories = {'1551_2023_q3': r'C:\Users\ptagal\Adaptive Biotechnologies\Environmental Health and Safety - Inspection Forms\Eyewash & Shower Inspections\1551\2023\Q3',
-                                                 '1551_2023_q4': r'C:\Users\ptagal\Adaptive Biotechnologies\Environmental Health and Safety - Inspection Forms\Eyewash & Shower Inspections\1551\2023\Q4',
-                                                 '1165_2023_q3': r'C:\Users\ptagal\Adaptive Biotechnologies\Environmental Health and Safety - Inspection Forms\Eyewash & Shower Inspections\1165\2023\Q3',
-                                                 '1165_2023_q4': r'C:\Users\ptagal\Adaptive Biotechnologies\Environmental Health and Safety - Inspection Forms\Eyewash & Shower Inspections\1165\2023\Q4',
-                                                 'bothell_2023_q3': r'C:\Users\ptagal\Adaptive Biotechnologies\Environmental Health and Safety - Inspection Forms\Eyewash & Shower Inspections\Bothell\2023\Q3',
-                                                 'bothell_2023_q4': r'C:\Users\ptagal\Adaptive Biotechnologies\Environmental Health and Safety - Inspection Forms\Eyewash & Shower Inspections\Bothell\2023\Q4'}
+destination_true_north_inspection_directories = {'1551_2023_q3': r'C:\Users\ptagal\Adaptive Biotechnologies\Environmental Health & Safety - Inspection Forms\Eyewash & Shower Inspections\1551\2023\Q3',
+                                                 '1551_2023_q4': r'C:\Users\ptagal\Adaptive Biotechnologies\Environmental Health & Safety - Inspection Forms\Eyewash & Shower Inspections\1551\2023\Q4',
+                                                 '1165_2023_q3': r'C:\Users\ptagal\Adaptive Biotechnologies\Environmental Health & Safety - Inspection Forms\Eyewash & Shower Inspections\1165\2023\Q3',
+                                                 '1165_2023_q4': r'C:\Users\ptagal\Adaptive Biotechnologies\Environmental Health & Safety - Inspection Forms\Eyewash & Shower Inspections\1165\2023\Q4',
+                                                 'bothell_2023_q3': r'C:\Users\ptagal\Adaptive Biotechnologies\Environmental Health & Safety - Inspection Forms\Eyewash & Shower Inspections\Bothell\2023\Q3',
+                                                 'bothell_2023_q4': r'C:\Users\ptagal\Adaptive Biotechnologies\Environmental Health & Safety - Inspection Forms\Eyewash & Shower Inspections\Bothell\2023\Q4'}
 
 
 source_directory_values = source_directories.values()
 destination_directory_values = destination_true_north_inspection_directories.values()
 
+for source_dir, destination_dir in zip(source_directory_values, destination_directory_values):
+    for root, _, files in os.walk(source_dir):
+        for file in files:
+            source_path = os.path.join(root, file)
+            destination_path = os.path.join(destination_dir, os.path.relpath(source_path, source_dir))
 
+            # Check if the file already exists in the destination directory
+            if not os.path.exists(destination_path):
+                os.makedirs(os.path.dirname(destination_path), exist_ok=True)
+                shutil.copy2(source_path, destination_path)
+                print(f"File '{file}' copied to '{destination_dir}'")
+            else:
+                print(f"File '{file}' already exists in '{destination_dir}' and was skipped")
 
+print("File copying o EHS TrueNorth sharepoint site directory completed.")
 
-# for i in source_directory_values:
-#     print(i)
-#
-# for directory in source_directory_values:
-#     for filename in os.listdir(directory):
-#         if filename.endswith(".pdf"):  # Check if the file is a PDF
-#             source_path = os.path.join(directory, filename)
-#             destination_path = os.path.join(destination_directory, filename)
-#             shutil.copy2(source_path, destination_path)
